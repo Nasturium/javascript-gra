882cd0dd87a00e2dd6db035418b4eaf43209805a
Aleksandra Rączka <aleksandraraczkaa@gmail.com>
2023-08-18 19:37:50 +0200

    add display result

--- js/script.js
+++ js/script.js
@@ -1,74 +1,70 @@
 function getMoveName(argMoveId){
-  if(argMoveId == 1){
+if(argMoveId == 1){
     return 'kamień';
+  } else if(argMoveId == 2) {
+    return 'papier';
+  } else if(argMoveId == 3) {
+    return 'nożyce';
   }
-
-  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
   return 'nieznany ruch';
 }
+
+
+
 function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
 
+
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
 
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove = getMoveName(randomNumber);
+let argComputerMove = getMoveName(randomNumber);
 
-/*if(randomNumber == 1){
-  computerMove = 'kamień';
-} /*else if (randomNumber == 2){
-  computerMove = 'papier';
-} else if(randomNumber == 3){
-  computerMove ='nożyce';
-}*/
 
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Mój ruch to: ' + argComputerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-console.log('Gracz wpisał: ' + playerInput);
+let argPlayerMove = getMoveName(playerInput);
 
-let playerMove = getMoveName(randomNumber);
+printMessage('Twój ruch to: ' + argPlayerMove);
 
-/*if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if(playerInput =='2'){
-  playerMove = 'papier';
-} else if(playerInput == '3'){
-  playerMove = 'nożyce';
-}*/
+let argMoveId = displayResult(argComputerMove, argPlayerMove);
 
-printMessage('Twój ruch to: ' + playerMove);
+function displayResult(argComputerMove, argPlayerMove){
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-  printMessage('Ty wygrywasz!');
-} else if(computerMove == 'kamień' && playerMove == 'kamień'){
+}if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    printMessage('Ty wygrywasz!');   
+} else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+    printMessage('REMIS');
+} else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+  printMessage('Przegrałeś!');
+}else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+  printMessage('Przegrałeś!');
+} else if( argComputerMove == 'papier' && argPlayerMove =='papier'){
   printMessage('REMIS');
-} else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+} else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
   printMessage('Przegrałeś!');
-}else if( computerMove == 'papier' && playerMove == 'kamień'){
-  printMessage('Przegrałeś!');
-} else if( computerMove == 'papier' && playerMove =='papier'){
-  printMessage('REMIS');
-} else if(computerMove == 'papier' && playerMove == 'nożyce'){
-  printMessage('Przegrałeś!');
-} else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+} else if(argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
   printMessage('REMIS!');
-} else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+} else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
   printMessage('Ty wygrywasz!');
-} else if(computerMove == 'nożyce' && playerMove == 'papier'){
+} else if(argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
   printMessage('Przegrałeś!');
-} else if(computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+} else if(argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch'){
   printMessage('nie wiem co robić! - wpisz 1, 2 lub 3')
-} else if(computerMove == 'papier' && playerMove == 'nieznany ruch'){
+} else if(argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch'){
   printMessage('nie wiem co robić! - wpisz 1, 2 lub 3')
-}  else if(computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+}  else if(argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch'){
   printMessage('nie wiem co robić! - wpisz 1, 2 lub 3')
 }
